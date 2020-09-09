5e996b95ea375b0e5105929ef16957cfffa533d5
ndzalama mabasa <ndzalamabasa@gmail.com>
2020-09-09 06:16:06 +0200

    more work on my profile

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const mobileMenu = document.querySelector(".desktop-nav");
 showMenu.addEventListener('click', function () {
     mobileMenu.classList.toggle("show");
 });
+
+window.addEventListener("scroll", () => {
+    const navigation = document.querySelector(".nav-bar");
+    const heading = document.querySelector(".welcome");
+    if (heading.pageYOffset==0) {
+        navigation.style.backgroundColor = "#000";
+    } 
+});
