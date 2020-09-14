b940ccd9c5358683b42853ff211769843bdae458
ndzalama mabasa <ndzalamabasa@gmail.com>
2020-09-14 23:11:19 +0200

    First deployed profile

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const showMenu = document.querySelector(".mobile-show");
 const mobileMenu = document.querySelector(".desktop-nav");
+const menuItems = document.querySelectorAll(".mobile-hide");
+const navigation = document.querySelector(".nav-bar");
 
 showMenu.addEventListener('click', function () {
     mobileMenu.classList.toggle("show");
+    navigation.style.backgroundColor = "#000";
 });
 
+for (let item of menuItems) {
+    item.addEventListener('click', function () {
+        mobileMenu.classList.remove("show");
+    });
+}
+
 window.addEventListener("scroll", () => {
-    const navigation = document.querySelector(".nav-bar");
+    
     const heading = document.querySelector(".welcome");
-    if (heading.pageYOffset==0) {
+    if ((window.scrollY)/350) {
         navigation.style.backgroundColor = "#000";
     } 
 });
