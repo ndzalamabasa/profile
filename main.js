f6b7f5f34f91f2ef8fe90b0748df1b3b5cfae13d
ndzalama mabasa <ndzalamabasa@gmail.com>
2020-11-17 12:53:22 +0200

    first revised version

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 const showMenu = document.querySelector(".mobile-menu");
-const mobileMenu = document.querySelector(".mobile-hide");
-// const menuItems = document.querySelectorAll(".mobile-hide");
-
-// const navigation = document.querySelector(".navigation");
+const mobileMenu = document.querySelector(".desktop-nav");
 
 showMenu.addEventListener('click', function () {
     mobileMenu.classList.toggle("show-menu");
-    // navigation.style.backgroundColor = "#000";
 });
-
-// for (let item of menuItems) {
-//     item.addEventListener('click', function () {
-//         mobileMenu.classList.remove("show");
-//     });
-// }
-
-// window.addEventListener("scroll", () => {
-    
-//     const heading = document.querySelector(".welcome");
-//     if ((window.scrollY)/350) {
-//         navigation.style.backgroundColor = "#000";
-//     } 
-// });
